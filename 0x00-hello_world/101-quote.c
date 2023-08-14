461b6bb200cefebf0b8fd87d02033034459ae846
Bnabdulfatai <abdulfataiadam3@gmail.com>
2023-08-14 15:18:45 -0700

    failure

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
-
+/**
+ * main - A program that has a unix head
+ * Return: 1(Failure)
 int main(void)
 {
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
