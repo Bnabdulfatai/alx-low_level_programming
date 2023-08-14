2d69461d5f0a8139ffc856573b952e58f53c1816
Bnabdulfatai <abdulfataiadam3@gmail.com>
2023-08-14 14:51:50 -0700

    various computer bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int i;
 long l;
 long long ll;
 float f;
-printf("Size of char: %lu bytes\n", (unsigned long)sizeof(c));
-printf("Size of int: %lu bytes\n", (unsigned long)sizeof(i));
-printf("Size of long: %lu bytes\n", (unsigned long)sizeof(l));
-printf("Size of long long: %lu bytes\n", (unsigned long)sizeof(ll));
-printf("Size of float: %lu bytes\n", (unsigned long)sizeof(f));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of long: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of long long: %lu byte(s)\n", (unsigned long)sizeof(ll));
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
