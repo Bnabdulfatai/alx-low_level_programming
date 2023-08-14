9bc9497b91d21b39b5b9e89a9df5ec20c2a9edc1
Bnabdulfatai <abdulfataiadam3@gmail.com>
2023-08-14 13:42:20 -0700

    printing a statement using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a program that prints using the printf function
+ * Return: 0 (if our program print 0 it means our project is a sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
