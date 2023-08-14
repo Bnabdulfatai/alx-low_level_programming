3eb5ea544de236d964d2932e5d87831153e76be1
Bnabdulfatai <abdulfataiadam3@gmail.com>
2023-08-14 13:25:03 -0700

    usage os the puts fumnction

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is a program that prints with puts function
+ * Return: 0 (when our project returns 0 it means our project is a sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
