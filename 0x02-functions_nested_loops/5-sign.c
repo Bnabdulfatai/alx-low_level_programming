7314d63ee78ad38af85d821e9ce09b2f073b04db
Bnabdulfatai <abdulfataiadam3@gmail.com>
2023-08-17 14:29:09 -0700

    5_signs) git push

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * main -  entry point
+ * description: checks code 
+ * Return: + if n>0, 0 if n=0, -1 if n<0
+ */
+int main(void)
+{
+	int print_sign(int n)
+	{
+
+		if (n>0)
+		{
+			_putchar('+');
+			_putchar('\n');
+			return (1);
+		}
+
+		if else (n == 0)
+		{
+			_putchar('0');
+	                _putchar('\n');
+			return (0);
+		}
+
+		else (n<0)
+		{
+			_putchar('-');
+			_putchar('/n');
+			return (-1);
+		}
+	}
+}
