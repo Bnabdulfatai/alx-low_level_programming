9481fc2fb13b289ea6131c27c82131d50e7484d6
Bnabdulfatai <abdulfataiadam3@gmail.com>
2023-08-17 16:57:38 -0700

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
-	/**
-	 *jack_bauer -  prints every minute of the day of Jack Bauer
-	 *  starting from 23:59
-	 */
+/**
+*jack_bauer -  prints every minute of the day of Jack Bauer
+*starting from 23:59
+*/
 void jack_bauer(void)
 {
 	int x, y;
