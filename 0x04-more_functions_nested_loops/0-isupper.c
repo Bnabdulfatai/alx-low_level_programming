fd175b37c78dbca5714bc58da30c0ad9814e1d78
Bnabdulfatai <abdulfataiadam3@gmail.com>
2023-08-20 09:39:03 -0700

    _isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"i
+/**
+ * _isupper - uppercase letters
+ * @c: character to check
+ * Retun: (0) or (1)
+ */
+int_isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return ('1');
+	else
+		return ('0');
